ae6baf5c92a5efd81d4e7753bb6ec0b6af759cfb
yury <yury.antonyb@gmail.com>
2021-10-10 16:05:41 -0400

    Initial Commit

---
+++ SUIMLCamera/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SUIMLCamera
+//
+//  Created by yury antony on 10/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SUIMLCamera/SUIMLCameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  SUIMLCameraApp.swift
+//  SUIMLCamera
+//
+//  Created by yury antony on 10/10/21.
+//
+
+import SwiftUI
+
+@main
+struct SUIMLCameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
